2e6045be5ffcd21c0ac1729d083273fcecc8a43b
glennin-codes <glen.nyandiri@students.kyu.co.ke>
2023-03-03 15:36:02 +0300

    starting up

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./App.css";
+
+import Products from "./components/Products";
+
+function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#0066cc',
+      },
+    },
+  });
+  
+
+  return (
+  <ThemeProvider theme={theme}>
+
+      {/* Your app components */}
+
+ <Products />
+
+    
+  
+    </ThemeProvider>
+  )
+}
+
+export default App;
