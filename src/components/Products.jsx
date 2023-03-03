00d5d973e5aa14cbb0850da58ab8a813cfadbed4
glennin-codes <glen.nyandiri@students.kyu.co.ke>
2023-03-03 16:17:35 +0300

    add product

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { 
+  Button,
+  Alert,
+  FormControl,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  Box,
+  Paper, } from '@mui/material';
+  
+  import React from 'react'
 
 export default function Products() {
+  const handleSubmit=()=>{
+  
+  }
   return (
-    <div>Productsnjkcv bjcv</div>)
-}
+    <>
+    <Typography variant="h4" align="center" color="primary" fontWeight="bold">
+      Add New product  In Shop
+    </Typography>
+    <Box maxWidth="sm" sx={{ my: 4, mx: "auto" }}>
+     
+        <form onSubmit={handleSubmit}>
+
+</form>
+</Box>
+</>
+         
+         )
+          }
 
 
