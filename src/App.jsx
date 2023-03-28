19ac91d86f70eee77c72a1f865e8eac6b340310f
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-28 05:03:58 +0300

    solved a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from "react";
-import "./App.css";
-
-import Products from "./components/Products.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Manageproducts from "./components/ManageProduct";
-import Login from "./components/Auth/Login";
-import SignUp from "./components/Auth/Signup";
-import VerifyEmail from "./components/Verification";
+import { BrowserRouter, Route, Routes } from '@router';
+import Products from '@components/Products';
+import Manageproducts from '@components/ManageProduct';
+import Login from '@components/Auth/Login';
+import SignUp from '@components/Auth/Signup';
+import VerifyEmail from '@components/Verification';
+import './App.css';
 
 function App() {
- 
-  
 
   return (
-
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Products />} />
-      <Route path="/manage" element={<Manageproducts />} />
-      <Route path="/auth/login" element={<Login />} />
-      <Route path="/auth/signup" element={<SignUp />} />
-      <Route path="/verifycode" element={<VerifyEmail />} />
-    </Routes>
-     
-     </BrowserRouter>
-
-  
-   
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/manage" element={<Manageproducts />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signup" element={<SignUp />} />
+        <Route path="/verifycode" element={<VerifyEmail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
