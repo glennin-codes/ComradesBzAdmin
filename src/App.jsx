a3dcbfa718963061b4b93dd20d9d6b343a20acb6
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-28 05:14:13 +0300

    solved a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { BrowserRouter, Route, Routes } from '@router';
-import Products from '@components/Products';
-import Manageproducts from '@components/ManageProduct';
-import Login from '@components/Auth/Login';
-import SignUp from '@components/Auth/Signup';
-import VerifyEmail from '@components/Verification';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import Products from "./components/Products.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Manageproducts from "./components/ManageProduct";
+import Login from "./components/Auth/Login";
+import SignUp from "./components/Auth/Signup";
+import VerifyEmail from "./components/Verification";
 
 function App() {
+ 
+  
 
   return (
+
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Products />} />
-        <Route path="/manage" element={<Manageproducts />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/signup" element={<SignUp />} />
-        <Route path="/verifycode" element={<VerifyEmail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Products />} />
+      <Route path="/manage" element={<Manageproducts />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/signup" element={<SignUp />} />
+      <Route path="/verifycode" element={<VerifyEmail />} />
+    </Routes>
+     
+     </BrowserRouter>
+
+  
+   
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+// import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '@components': resolve(__dirname, './src/components')
-    },
+  
   },
 
-});
+);
